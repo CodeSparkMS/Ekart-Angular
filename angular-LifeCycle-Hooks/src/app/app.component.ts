9aba88e37b5aa4689dd1719bec9ab54638275968
SaimMohanish <mohanish.saim@zingworks.in>
2024-05-28 17:07:34 +0530

    - learning the ngOnChanges() life cycle hook  - learning the ngOnInit() life cycle hook  - learning the ngDoCheck() life cycle hook

--- angular-LifeCycle-Hooks/src/app/app.component.ts
+++ angular-LifeCycle-Hooks/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-LifeCycle-Hooks';
 
-  constructor(){
-    console.log("App component is called")
+  constructor() {
+    console.log('App component is called');
   }
 
+  text: string = '';
+
+  // @ViewChild('inputVal') parValRef: ElementRef;    // We can also use @ViewChild() here
+
+  inputValueFun(inputEl: HTMLInputElement) {
+    // console.log(inputEl.value);
+    this.text = inputEl.value;
+    // console.log('parValRef...', this.parValRef.nativeElement.value);
+  }
 }
--- angular-LifeCycle-Hooks/src/app/demo/demo.component.ts
+++ angular-LifeCycle-Hooks/src/app/demo/demo.component.ts
@@ -1,18 +1,47 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css'],
 })
-export class DemoComponent {
+export class DemoComponent implements OnChanges, OnInit, DoCheck {
   testVar1: string = 'Hello Mohanish';
 
-  @Input() testVar2: string = 'Hi';
+  // @Input() testVar2: string = 'Hi';
 
   constructor() {
     console.log('Demo component is called');
     console.log('this.testVar1...', this.testVar1);
-    console.log('this.testVar2...', this.testVar2);
+    // console.log('this.testVar2...', this.testVar2);
+  }
+
+  @Input() parentMessage: string = '';
+
+  @ViewChild('temp') msgRef: ElementRef;
+
+  // ngOnChanges() Life Cycle Hook
+  ngOnChanges() {
+    console.log('ngOnChange called....');
+    console.log('parentMessage...', this.parentMessage);
+  }
+
+  //ngOnInit() Life Cycle Hook
+  ngOnInit() {
+    console.log('ngOnInit called...');
+    // console.log('ngOnInit...msgRef...msgRef...msgRef',this.msgRef.nativeElement); // It gives error as @ViewChild() decorated property is not available
+  }
+
+  //ngDoCheck Life Cycle Hook
+  ngDoCheck() {
+    console.log('ngDoCheck called....');
   }
 }
