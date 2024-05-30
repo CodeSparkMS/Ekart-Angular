dfed617ff98d5df4001ab51524cfa21e33d81857
SaimMohanish <mohanish.saim@zingworks.in>
2024-05-30 11:27:23 +0530

    - Use of custom attribute directive to manipulate DOM elelment - use of ngOnInit hook to manipulate DOM element - use of private keyword to make constructor variable accessible in class

--- angular-ekart/src/app/container/CustomDirective/SetBackground.directive.ts
+++ angular-ekart/src/app/container/CustomDirective/SetBackground.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
-export class SetBackground {
-  private element: ElementRef;
-  constructor(element: ElementRef) {
-    // element.nativeElement.style.backgroundColor = '#28282B';
-    // element.nativeElement.style.color = 'white';
-    this.element = element;
-  }
+export class SetBackground implements OnInit {
+  //   private element: ElementRef;
+  //   constructor(element: ElementRef) {
+  //     // element.nativeElement.style.backgroundColor = '#28282B';
+  //     // element.nativeElement.style.color = 'white';
+  //     this.element = element;
+  //   }
+
+  // OR
+  constructor(private element: ElementRef) {}
 
   ngOnInit() {
     this.element.nativeElement.style.backgroundColor = '#28282B';
