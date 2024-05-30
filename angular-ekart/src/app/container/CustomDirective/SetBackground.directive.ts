ca45ff6012c3c32405dbcd3e3937ca41483ff3eb
SaimMohanish <mohanish.saim@zingworks.in>
2024-05-30 11:41:10 +0530

    - Use of Render2 to manipulate the DOM element without accessing it directly

--- angular-ekart/src/app/container/CustomDirective/SetBackground.directive.ts
+++ angular-ekart/src/app/container/CustomDirective/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
@@ -12,10 +12,16 @@ export class SetBackground implements OnInit {
   //   }
 
   // OR
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef, private render: Renderer2) {}
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#28282B';
-    this.element.nativeElement.style.color = 'white';
+    // this.element.nativeElement.style.backgroundColor = '#28282B';
+    // this.element.nativeElement.style.color = 'white';
+
+    // USE OF Renderer2 to manipulate the DOM element withpout accessing it directly
+    this.render.setStyle(this.element.nativeElement,'backgroundColor','green')
+    this.render.setStyle(this.element.nativeElement, 'color','white')
   }
+
+
 }
